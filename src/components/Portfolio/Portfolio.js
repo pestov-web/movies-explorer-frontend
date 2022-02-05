8bd48452fb5bc0efbd08ab2d40c8336ee6a1187f
vladimir pestov <pestov.web@yandex.ru>
2022-02-05 12:31:47 +1000

    fix/ links _blank

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -9,6 +9,8 @@ function Portfolio() {
           <a
             href="https://github.com/pestov-web/how-to-learn"
             className="portfolio__link link"
+            target="_blank"
+            rel="noreferrer"
           >
             Статичный сайт
             <div className="portfolio__link-icon"></div>
@@ -18,13 +20,20 @@ function Portfolio() {
           <a
             href="https://github.com/pestov-web/russian-travel"
             className="portfolio__link link"
+            target="_blank"
+            rel="noreferrer"
           >
             Адаптивный сайт
             <div className="portfolio__link-icon"></div>
           </a>
         </li>
         <li className="portfolio__item">
-          <a href="https://pestov-web.ru/" className="portfolio__link link">
+          <a
+            href="https://pestov-web.ru/"
+            className="portfolio__link link"
+            target="_blank"
+            rel="noreferrer"
+          >
             Одностраничное приложение
             <div className="portfolio__link-icon"></div>
           </a>
