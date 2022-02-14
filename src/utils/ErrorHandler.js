83a2a9242bcaffa0f4e74ba4d37b3b0115fbd0dd
vladimir pestov <pestov.web@yandex.ru>
2022-02-14 13:18:52 +1000

    fix: errors

--- src/utils/ErrorHandler.js
+++ src/utils/ErrorHandler.js
@@ -1,8 +1,11 @@
 const AUTH_ERROR_MSG = 'Ошибка авторизации';
-
+const SERVER_ERROR_MSG = 'Сервер недоступен, попробуйте позже';
 export const ErrorHandler = (error) => {
   console.log(`Ошибка : ${error}`);
   if (error == 401) {
     console.log(AUTH_ERROR_MSG);
+  }
+  if (error == 'TypeError: Failed to fetch') {
+    console.log(SERVER_ERROR_MSG);
   }
 };
