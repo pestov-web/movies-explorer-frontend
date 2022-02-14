4c7b3e27569acc616be956feba902d1be092c153
vladimir pestov <pestov.web@yandex.ru>
2022-02-14 13:15:02 +1000

    fix: errors

--- src/utils/ErrorHandler.js
+++ src/utils/ErrorHandler.js
@@ -1,3 +1,8 @@
+const AUTH_ERROR_MSG = 'Ошибка авторизации';
+
 export const ErrorHandler = (error) => {
-  console.log(error);
+  console.log(`Ошибка : ${error}`);
+  if (error == 401) {
+    console.log(AUTH_ERROR_MSG);
+  }
 };
