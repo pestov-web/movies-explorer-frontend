f708d3d9e55838bd0ab92e553689b5ef8f573e0b
vladimir pestov <pestov.web@yandex.ru>
2022-02-17 01:39:43 +1000

    fix: redirect

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -4,7 +4,15 @@ import { Route, Redirect } from 'react-router-dom';
 const ProtectedRoute = ({ component: Component, ...props }) => {
   return (
     <Route>
-      {props.loggedIn ? <Component {...props} /> : <Redirect to="/" />}
+      {
+        // props.loggedIn ? <Component {...props} /> : <Redirect to="/" />
+        () =>
+          props.loggedIn === true ? (
+            <Component {...props} />
+          ) : (
+            <Redirect to="/" />
+          )
+      }
     </Route>
   );
 };
