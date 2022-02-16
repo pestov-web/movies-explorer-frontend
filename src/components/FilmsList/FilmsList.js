a64505aabadbc63b16cce36422fa92a758728b8f
vladimir pestov <pestov.web@yandex.ru>
2022-02-17 00:39:05 +1000

    fix: movies likes

--- src/components/FilmsList/FilmsList.js
+++ src/components/FilmsList/FilmsList.js
@@ -37,13 +37,7 @@ function FilmsList({ currenPath, movies, onSave, onRemove }) {
   const checkSaved = (movie) => {
     const movieArray = localStorageHandler.get('savedMovies');
 
-    if (movieArray.find((el) => el.movieId === movie.movieId)) {
-      console.log('true');
-    } else {
-      console.log('false');
-    }
-
-    return true;
+    return !!movieArray.find((el) => el.movieId === movie.movieId);
   };
 
   React.useEffect(() => {
